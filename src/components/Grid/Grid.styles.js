2063dfb58f2373ab25541c2f662cf44f7da386ae
jayden626 <jayden_626@hotmail.com>
2021-09-05 13:01:13 +1000

    Fixed mobile sizing

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -27,4 +27,8 @@ export const Content = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(22rem, 1fr));
     grid-gap: 1rem;
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+    }
 `;
